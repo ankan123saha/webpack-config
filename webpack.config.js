e043009eb5261a2da4ea24267358bb4c878dedde
asaha2 <asaha@grepruby.io>
2021-10-10 01:40:08 +0530

    css added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const mode=process.env.NODE_ENV === 'production' ? 'production' : 'development'
 module.exports={
     mode: mode,
+    target: 'browserslist',
     devtool: 'source-map',
     module: {
       rules:[
@@ -11,9 +13,15 @@ module.exports={
           use: {
             loader: 'babel-loader'
           }
+        },
+        {
+          test: /\.s?css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader",]
+
         }
       ]
     },
+    plugins: [new MiniCssExtractPlugin()],
     devServer: {
         static: {
           directory: path.join(__dirname, 'dist'),

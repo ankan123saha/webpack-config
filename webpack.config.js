219a305c10c441260c2b6482982862491ff45bcb
asaha2 <asaha@grepruby.io>
2021-10-10 21:00:11 +0530

    image loader added

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,18 @@ module.exports={
     mode: mode,
     target: 'browserslist',
     devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]'
+    },
     module: {
       rules:[
+        { 
+          test: /\.(png|jpe?g|svg)$/i,
+          type: "asset/inline"
+        },
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
